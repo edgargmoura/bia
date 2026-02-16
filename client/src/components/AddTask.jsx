17c7da852c4181ab6c19755a68f30cdbcdab9906
EC2 Default User <ec2-user@ip-172-31-15-22.ec2.internal>
2026-02-16 22:00:18 +0000

    Ajustando texto no botão

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Nova Task
+        Add New Task
       </button>
       
       <Modal
